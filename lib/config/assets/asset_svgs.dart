9e1be66f21ada435e176f6789bb2918800140504
imsisojib <imsisojib@gmail.com>
2026-03-04 15:23:26 +0600

    bottomnav assets added.

---
+++ lib/config/assets/asset_svgs.dart
@@ -0,0 +1,6 @@
+final class AssetSvgs {
+  static const String discover = 'assets/svgs/bottomnav/discover.svg';
+  static const String home = 'assets/svgs/bottomnav/home.svg';
+  static const String portfolio = 'assets/svgs/bottomnav/portfolio.svg';
+  static const String profile = 'assets/svgs/bottomnav/profile.svg';
+}
