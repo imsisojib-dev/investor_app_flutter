cb99cfd1d012542054b3f400e8a993b7733fc8eb
imsisojib <imsisojib@gmail.com>
2026-03-05 14:49:17 +0600

    custom card widget added.

---
+++ lib/src/shared/widgets/custom_card.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:investor_app_flutter/src/core/theme/app_theme_extension.dart';
+
+class CustomCard extends StatelessWidget {
+  final Color? backgroundColor;
+  final Widget child;
+  final EdgeInsetsGeometry? padding;
+
+  const CustomCard({super.key, required this.child, this.padding, this.backgroundColor});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 16),
+      padding: padding ?? const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: backgroundColor ?? theme.appColors.white,
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: [BoxShadow(color: Colors.black.withOpacity(0.04), blurRadius: 8, offset: const Offset(0, 2))],
+      ),
+      child: child,
+    );
+  }
+}
