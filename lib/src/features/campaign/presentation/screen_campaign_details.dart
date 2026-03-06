2e9f52311325d4d6816c5ec886d2d1f8581ae5fc
imsisojib <imsisojib@gmail.com>
2026-03-06 14:12:50 +0600

    unnecessary code removed.

--- lib/src/features/campaign/presentation/screen_campaign_details.dart
+++ lib/src/features/campaign/presentation/screen_campaign_details.dart
@@ -4,7 +4,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:investor_app_flutter/src/config/extensions/widget_extensions.dart';
 import 'package:investor_app_flutter/src/core/theme/app_colors.dart';
 import 'package:investor_app_flutter/src/core/theme/app_theme_extension.dart';
-import 'package:investor_app_flutter/src/core/utils/widget_helper.dart';
+import 'package:investor_app_flutter/src/core/helpers/widget_helper.dart';
 import 'package:investor_app_flutter/src/shared/widgets/custom_card.dart';
 import 'package:investor_app_flutter/src/shared/widgets/dotted_line.dart';
 
