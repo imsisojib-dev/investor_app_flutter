171a64ebeee19fbf222daab1d514c883bd4c7571
imsisojib <imsisojib@gmail.com>
2026-03-30 22:55:10 +0600

    generic exceptions defined.

--- lib/src/core/network/api_client.dart
+++ lib/src/core/network/api_client.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
+import 'package:investor_app_flutter/src/core/errors/exceptions.dart';
 
 import 'dio_client.dart';
 
@@ -28,7 +29,7 @@ class ApiClient implements IApiClient {
     try {
       return await _dio.get(path, queryParameters: queryParameters);
     } on DioException catch (e) {
-      throw Exception(e);
+      throw _handleDioException(e);
     }
   }
 
@@ -37,7 +38,7 @@ class ApiClient implements IApiClient {
     try {
       return await _dio.post(path, data: data);
     } on DioException catch (e) {
-      throw Exception(e);
+      throw _handleDioException(e);
     }
   }
 
@@ -46,7 +47,7 @@ class ApiClient implements IApiClient {
     try {
       return await _dio.put(path, data: data);
     } on DioException catch (e) {
-      throw Exception(e);
+      throw _handleDioException(e);
     }
   }
 
@@ -55,7 +56,7 @@ class ApiClient implements IApiClient {
     try {
       return await _dio.patch(path, data: data);
     } on DioException catch (e) {
-      throw Exception(e);
+      throw _handleDioException(e);
     }
   }
 
@@ -64,7 +65,7 @@ class ApiClient implements IApiClient {
     try {
       return await _dio.delete(path);
     } on DioException catch (e) {
-      throw Exception(e);
+      throw _handleDioException(e);
     }
   }
 
@@ -82,7 +83,47 @@ class ApiClient implements IApiClient {
         options: Options(headers: {'Content-Type': 'multipart/form-data'}),
       );
     } on DioException catch (e) {
-      throw Exception(e);
+      throw _handleDioException(e);
     }
   }
+
+  AppException _handleDioException(DioException e) {
+    switch (e.type) {
+      case DioExceptionType.connectionTimeout:
+      case DioExceptionType.sendTimeout:
+      case DioExceptionType.receiveTimeout:
+        return TimeoutException(message: 'Request timed out. Please try again.');
+
+      case DioExceptionType.connectionError:
+        return NetworkException(message: 'No internet connection.');
+
+      case DioExceptionType.badResponse:
+        return _handleStatusCode(e.response?.statusCode, e.response?.data);
+
+      case DioExceptionType.cancel:
+        return RequestCancelledException(message: 'Request was cancelled.');
+
+      default:
+        return UnknownException(message: 'Something went wrong. Please try again.');
+    }
+  }
+
+  AppException _handleStatusCode(int? statusCode, dynamic data) {
+    final message = data?['message'] as String? ?? 'Unknown error occurred';
+
+    switch (statusCode) {
+      case 400: return BadRequestException(message: message);
+      case 401: return UnauthorizedException(message: message);
+      case 403: return ForbiddenException(message: message);
+      case 404: return NotFoundException(message: message);
+      case 409: return ConflictException(message: message);
+      case 422: return ValidationException(message: message, errors: data?['errors']);
+      case 429: return TooManyRequestsException(message: message);
+      case 500:
+      case 502:
+      case 503: return ServerException(message: message);
+      default:  return UnknownException(message: message);
+    }
+  }
+
 }
